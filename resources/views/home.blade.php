defc65d07e47c583013276b6be8f70e7e9d1ce69
Dicky Hung <dickyhung12@gmail.com>
2022-11-05 10:41:43 +0700

    initial commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,20 @@
-@extends('isi')
+@extends('template')
 
 @section('content')
-    @for ($i = 1; $i <= 5; $i++)   
-        <div class="text-center text-white bg-danger rounded-5" style="margin: 10px 30%">
-            <h4>Ini adalah loop ke {{$i}}</h4>
-            @if ($i != 5)
-                <h4>Yang akan dilanjutkan dengan {{$i+1}}</h4>
-            @elseif ($i == 5)
-                <h4>Yang Merupakan loop terakhir</h4>
-            @endif    
+    <div class= "bg-black" style="height: 500px">
+        <div class="d-flex">
+            @for ($i = 1; $i <= 5; $i++)   
+            <div class="text-center text-white bg-primary rounded-5 m-2 pt-3" style="">
+                <img src="{{$img}}" alt="" class="w-75 rounded-5">
+                <h4>Ini adalah loop ke {{$i}}</h4>
+                @if ($i != 5)
+                    <h4>Yang akan dilanjutkan dengan {{$i+1}}</h4>
+                @elseif ($i == 5)
+                    <h4>Yang Merupakan loop terakhir</h4>
+                @endif    
+            </div>
+        @endfor
         </div>
-    @endfor
+    </div>
+    
 @endsection
